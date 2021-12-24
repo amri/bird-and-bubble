7b142bc7bc81b42d53f108c9f10bb5e975890c2b
Amri Hidayat <amri.hidayat@gmail.com>
2021-12-24 16:33:55 +0800

    add lava

--- main.ts
+++ main.ts
@@ -1,35 +1,180 @@
-function createBubble (level: number) {
-    bubble = sprites.create(img`
-        . . . . . b b b b b b . . . . . 
-        . . . b b 9 9 9 9 9 9 b b . . . 
-        . . b b 9 9 9 9 9 9 9 9 b b . . 
-        . b b 9 d 9 9 9 9 9 9 9 9 b b . 
-        . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
-        b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
-        b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
-        b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
-        b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
-        b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
-        b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
-        . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
-        . b d 5 3 3 3 3 3 3 3 d 5 b b . 
-        . . b d 5 d 3 3 3 3 5 5 b b . . 
-        . . . b b 5 5 5 5 5 5 b b . . . 
-        . . . . . b b b b b b . . . . . 
-        `, SpriteKind.Food)
-    newX = level * 0
-    bubble.setPosition(randint(10, 150), randint(10, 110))
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(isInitialized)) {
+        info.startCountdown(25)
+    }
+    isInitialized = true
+})
+info.onCountdownEnd(function () {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundCenter, function (sprite, location) {
+    if (bird.isHittingTile(CollisionDirection.Right) && info.score() == 0) {
+        if (level == 3) {
+            game.over(true)
+        }
+    }
+})
+function createLava () {
+    if (level == 1) {
+        lava = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . 4 4 4 5 5 4 4 4 . . . . 
+            . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+            . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+            . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+            . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+            . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+            . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+            . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+            . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+            . . . . 4 4 2 2 2 2 4 4 . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        new_lower_x = 10
+        new_upper_x = 150
+        lava.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
+    if (level == 2) {
+        lava = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . 4 4 4 5 5 4 4 4 . . . . 
+            . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+            . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+            . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+            . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+            . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+            . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+            . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+            . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+            . . . . 4 4 2 2 2 2 4 4 . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        new_lower_x = 160
+        new_upper_x = 300
+        lava.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
+    if (level == 3) {
+        lava = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . 4 4 4 5 5 4 4 4 . . . . 
+            . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+            . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+            . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+            . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+            . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+            . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+            . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+            . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+            . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+            . . . . 4 4 2 2 2 2 4 4 . . . . 
+            . . . . . . 4 4 4 4 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        new_lower_x = 310
+        new_upper_x = 450
+        lava.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
+}
+function createBubble () {
+    if (level == 1) {
+        bubble = sprites.create(img`
+            . . . . . b b b b b b . . . . . 
+            . . . b b 9 9 9 9 9 9 b b . . . 
+            . . b b 9 9 9 9 9 9 9 9 b b . . 
+            . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+            . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+            b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+            b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+            b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+            . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+            . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+            . . b d 5 d 3 3 3 3 5 5 b b . . 
+            . . . b b 5 5 5 5 5 5 b b . . . 
+            . . . . . b b b b b b . . . . . 
+            `, SpriteKind.Food)
+        new_lower_x = 10
+        new_upper_x = 150
+        bubble.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
+    if (level == 2) {
+        bubble = sprites.create(img`
+            . . . . . b b b b b b . . . . . 
+            . . . b b 9 9 9 9 9 9 b b . . . 
+            . . b b 9 9 9 9 9 9 9 9 b b . . 
+            . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+            . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+            b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+            b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+            b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+            . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+            . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+            . . b d 5 d 3 3 3 3 5 5 b b . . 
+            . . . b b 5 5 5 5 5 5 b b . . . 
+            . . . . . b b b b b b . . . . . 
+            `, SpriteKind.Food)
+        new_lower_x = 160
+        new_upper_x = 300
+        bubble.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
+    if (level == 3) {
+        bubble = sprites.create(img`
+            . . . . . b b b b b b . . . . . 
+            . . . b b 9 9 9 9 9 9 b b . . . 
+            . . b b 9 9 9 9 9 9 9 9 b b . . 
+            . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+            . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+            b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+            b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+            b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+            b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+            b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+            . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+            . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+            . . b d 5 d 3 3 3 3 5 5 b b . . 
+            . . . b b 5 5 5 5 5 5 b b . . . 
+            . . . . . b b b b b b . . . . . 
+            `, SpriteKind.Food)
+        new_lower_x = 310
+        new_upper_x = 450
+        bubble.setPosition(randint(new_lower_x, new_upper_x), randint(10, 110))
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     otherSprite.destroy(effects.bubbles, 300)
     bubblePopped += -1
     info.setScore(bubblePopped)
 })
-let newX = 0
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
 let bubble: Sprite = null
+let new_upper_x = 0
+let new_lower_x = 0
+let lava: Sprite = null
+let isInitialized = false
 let bubblePopped = 0
+let bird: Sprite = null
+let level = 0
 tiles.setTilemap(tilemap`level0`)
-let bird = sprites.create(img`
+level = 1
+bird = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . b 5 5 b . . . 
@@ -51,8 +196,11 @@ controller.moveSprite(bird, 100, 100)
 bird.setPosition(10, 10)
 scene.cameraFollowSprite(bird)
 for (let index = 0; index <= 4; index++) {
-    createBubble(1)
+    createBubble()
     bubblePopped = index
+}
+for (let index = 0; index <= 1; index++) {
+    createLava()
 }
 animation.runImageAnimation(
 bird,
@@ -164,12 +312,26 @@ true
 )
 game.onUpdate(function () {
     if (bird.isHittingTile(CollisionDirection.Right) && info.score() == 0) {
-        for (let index = 0; index <= 7; index++) {
-            tiles.setWallAt(tiles.getTileLocation(9, index), false)
-        }
-        for (let index = 0; index <= 5; index++) {
-            createBubble(1)
-            bubblePopped = index
+        if (level < 3) {
+            level += 1
+            game.splash("Welcome to level" + level + " !!!")
+            if (level == 2) {
+                for (let index = 0; index <= 7; index++) {
+                    tiles.setWallAt(tiles.getTileLocation(9, index), false)
+                }
+            }
+            if (level == 3) {
+                for (let index = 0; index <= 7; index++) {
+                    tiles.setWallAt(tiles.getTileLocation(19, index), false)
+                }
+            }
+            for (let index = 0; index <= 5; index++) {
+                createBubble()
+                bubblePopped = index
+            }
+            for (let index = 0; index <= 1; index++) {
+                createLava()
+            }
         }
     }
     info.setScore(sprites.allOfKind(SpriteKind.Food).length)
